87e5d16c5ce8df3a0f3d84cc21ecc270d132b0e4
danispinxo <genericpronoun@gmail.com>
2022-08-29 14:18:19 +0000

    adding prototype seeds and startign queries

--- db/seeds/03_qandas.sql
+++ db/seeds/03_qandas.sql
@@ -5,13 +5,13 @@ INSERT INTO answer (question_id, content, correct) VALUES (1, 'Farsi', FALSE);
 INSERT INTO answer (question_id, content, correct) VALUES (1, 'Pig Latin', FALSE);
 
 INSERT INTO question (quiz_id, content) VALUES (1, 'Which of the following breeds have often been used as mountain recuse dogs?');
-INSERT INTO answer (question_id, content, correct) VALUES (1, 'German Shepherd', FALSE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, 'Chihuahua', FALSE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, 'St. Bernards', TRUE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, 'Bassett Hounds', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (2, 'German Shepherd', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (2, 'Chihuahua', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (2, 'St. Bernards', TRUE);
+INSERT INTO answer (question_id, content, correct) VALUES (2, 'Bassett Hounds', FALSE);
 
 INSERT INTO question (quiz_id, content) VALUES (1, 'How many dogs did George Washington have in total?');
-INSERT INTO answer (question_id, content, correct) VALUES (1, '3', FALSE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, '7', FALSE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, '25', FALSE);
-INSERT INTO answer (question_id, content, correct) VALUES (1, '36', TRUE);
+INSERT INTO answer (question_id, content, correct) VALUES (3, '3', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (3, '7', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (3, '25', FALSE);
+INSERT INTO answer (question_id, content, correct) VALUES (3, '36', TRUE);
