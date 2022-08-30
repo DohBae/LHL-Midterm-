aa986062d1710ad975226fac1efb050b7477028b
DohBae <bdoherty92@msn.com>
2022-08-30 14:56:50 +0000

    Add form and radio buttons to display quiz questions and answers

--- routes/take-a-quiz.js
+++ routes/take-a-quiz.js
@@ -3,8 +3,8 @@ const router  = express.Router();
 const db = require('../db/connection');
 
 
-router.get('/views/take-a-quiz', (req, res) => {
-  console.log("it works")
+router.get('/', (req, res) => {
+  res.render('take-a-quiz');
 });
 
 module.exports = router;
