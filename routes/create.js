5c402184b101d664346f8f43ef5e501e605d4aa6
danispinxo <genericpronoun@gmail.com>
2022-08-30 14:56:38 +0000

    addAnswer query now works

--- routes/create.js
+++ routes/create.js
@@ -9,25 +9,31 @@ router.get('/', (req, res) => {
 });
 
 router.post('/new', (req, res) => {
+  // quiz variables
   let listed = true;
-
   if (req.body['public-private'] === 'Private') {
     listed = false;
   }
-// add quiz to db
   const quiz_id = generateRandomNumber();
   const creator_id = 1;
   const quizTitle = req.body.title;
   const quizDescription = req.body.description;
   const imageURL = req.body['img-url'];
 
-// add question to db
+  // question variables
   const questionText = req.body['question-text'];
 
+  // answer variables
   const answer1 = req.body['Answer-1'];
   const answer2 = req.body['Answer-2'];
   const answer3 = req.body['Answer-3'];
   const answer4 = req.body['Answer-4'];
+
+  console.log(req.body['answer-1-val']);
+  console.log(req.body['answer-2-val']);
+  console.log(req.body['answer-3-val']);
+  console.log(req.body['answer-4-val']);
+
 
   let answer1value = false;
   if (req.body['answer-1-val'] === 'Correct') {
@@ -49,16 +55,16 @@ router.post('/new', (req, res) => {
     answer1value = true;
   }
 
-
+// because these functions return promises (found in db/queries/create), they have to be run async
   addQuiz(quiz_id, creator_id, listed, quizTitle, quizDescription, imageURL)
     .then(() => {
       addQuestion(quiz_id, questionText);
     })
     .then(() => {
-      addAnswer(1, answer1, answer1value);
-      addAnswer(1, answer2, answer2value);
-      addAnswer(1, answer3, answer3value);
-      addAnswer(1, answer4, answer4value);
+      addAnswer(quiz_id, 1, answer1, answer1value);
+      addAnswer(quiz_id, 1, answer2, answer2value);
+      addAnswer(quiz_id, 1, answer3, answer3value);
+      addAnswer(quiz_id, 1, answer4, answer4value);
     });
 
   res.redirect('/');
