711659086c2fb4e3a6abccb5e71c29bd6a32cf73
danispinxo <genericpronoun@gmail.com>
2022-08-31 12:47:44 +0000

    setting up login and register queries and connecting to routes

--- db/queries/users.js
+++ db/queries/users.js
@@ -14,4 +14,13 @@ const getUserByEmail = (email) => {
     });
 };
 
-module.exports = { getUsers, getUserByEmail };
+const addUser = (username, email, password) => {
+  return db.query(`INSERT INTO users (username, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;`, [username, email, password])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getUsers, getUserByEmail, addUser };
--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 const db = require('../db/connection');
+const { addUser } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   res.render('register');
+});
+
+router.post('/', (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  addUser(username, email, password)
+    .then(() => {
+      res.redirect('/');
+    })
+
 });
 
 module.exports = router;
